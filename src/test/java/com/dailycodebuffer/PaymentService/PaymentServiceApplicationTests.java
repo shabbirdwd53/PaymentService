6e97c0cfb7abc62f825e2098da814b2d9de6e6d7
Shabbir Dawoodi <shabbirdwd53@gmail.com>
2022-10-15 12:00:26 +0530

    Initial Setup

---
+++ src/test/java/com/dailycodebuffer/PaymentService/PaymentServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dailycodebuffer.PaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
